2ad947cf45824b6b8d48b814b2c4423b27b111c0
AbdollahSafari <abdollahsafari1986@gmail.com>
2023-10-23 21:41:15 +0330

    Update User.cs

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain;
 
@@ -16,18 +17,29 @@ public class User : SeedWork.Entity
 
     #region 
 
-
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 40, MinimumLength = 3)]
     public string? Name { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 60, MinimumLength = 3)]
     [DisplayName ("Last Name")]
     public string? LastName { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 60, MinimumLength = 3)]
     public string? Position { get; set; }
 
+    [RegularExpression(pattern: "^\\d{11}$")]
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 11, MinimumLength = 5)]
     [Browsable(false)]
     [DisplayName("Office Phone Number")]
     public string? OfficePhone { get; set; }
 
+    [RegularExpression(pattern: "^\\d{11}$")]
+    [Required(AllowEmptyStrings = false)]
+    [StringLength(maximumLength: 11, MinimumLength = 9)]
     [Browsable (false)]
     [DisplayName("Mobile Number")]
     public string? MobilePhone { get; set; }
